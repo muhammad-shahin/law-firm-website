1ff4f371d902b4d6a0395763d4521936fc59445f
muhammad-shahin <muhammadshahin002@gmail.com>
2023-07-24 19:28:22 +0600

    initial setup done

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      'primaryBtn': "#B68C5A",
+      'secondary': '#414040',
+      'des': '#707070',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
